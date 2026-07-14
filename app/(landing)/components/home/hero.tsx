8f38591d8efad587170b5dee3018c70ccb57423c
janitrafs <jxnitraa0100@gmail.com>
2026-07-14 18:42:19 +0700

    push

---
+++ app/(landing)/components/home/hero.tsx
@@ -0,0 +1,50 @@
+import { FiFastForward } from "react-icons/fi";
+import Button from "../ui/button";
+
+const HeroSection = () => {
+    return (
+        <section id="hero-section" className="container mx-auto h-screen flex">
+            <div className="relative self-center">
+                <img src="/images/img-basketball.png" 
+                width={432}
+                height={423}
+                className="grayscale absolute left-0 -top-20"/>
+                <div className="relative ml-40 w-full">
+                    <div className="text-primary italic">Friday Sale, 50%</div>
+                <h1 className="font-extrabold text-[95px] italic leading-tight bg-gradient-to-b from-black to-[#979797D1] bg-clip-text text-transparent">
+                    WEAR YOUR <br /> TOP-QUALITY <br /> SPORTSWEAR
+                </h1>
+                <p className="w-1/2 mt-10 leading-loose">
+                    Engineered for endurance and designed for speed. Experience gear 
+                    that moves as fast as you do. Premium fabrics. Unmatched comfort. 
+                    Limitless motion.
+                </p>
+                <div className="flex gap-5 mt-8">
+                    <Button>
+                        Explore More <FiFastForward />
+                    </Button>
+                    <Button  variant="ghost">
+                        Watch Video <img src= "/Images/icon-play-video.svg" 
+                        alt="icon-play-video" 
+                        width={29} 
+                        height={29} />
+                    </Button>
+                </div>
+                </div>
+               <img src="/images/img-hero.png" 
+               width={700}
+               height={950}
+               alt="image-sport-on-hero"
+               className="absolute -right-5 top-1/2 -translate-y-1/2" /> 
+            </div>
+            <img src="/images/img-ornament-hero.svg" 
+            width={430} 
+            height={430}
+            alt="image-ornament-sporton"
+            className="absolute -right-[200px] top-1/2 -translate-y-1/2" />
+        </section>
+    )
+
+};
+
+export default HeroSection;
---
+++ app/(landing)/components/home/products.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../ui/button";
+import { FiPlus } from "react-icons/fi";
+
+const productList = [
+    {
+        name: "SportsOn Purple Shirt",
+        category: "Running",
+        price: 119000,
+        imgUrl: "product-1-kaos-ungu.png"
+    },
+    {
+        name: "SportsOn Purple Tennis Bundle",
+        category: "Tennis",
+        price: 999000,
+        imgUrl: "product-2-tennis-ungu.png"
+    },
+    {
+        name: "SportsOn Red Running Shoes",
+        category: "Running",
+        price: 329000,
+        imgUrl: "product-3-running-merah.png"
+    },
+    {
+        name: "SportsOn Red Football Shoes",
+        category: "Football",
+        price: 458000,
+        imgUrl: "product-4-football-merah.png"
+    },
+    {
+        name: "SportsOn Black Shirt",
+        category: "Running",
+        price: 119000,
+        imgUrl: "product-5-kaos-hitam.png"
+    },
+    {
+        name: "SportsOn Basketball",
+        category: "Basketball",
+        price: 150000,
+        imgUrl: "product-6-basket.png"
+    },
+    {
+        name: "SportsOn Green Tennis Bundle",
+        category: "Tennis",
+        price: 999000,
+        imgUrl: "product-7-tennis-ijo.png"
+    },
+    {
+        name: "SportsOn White Running Shoes",
+        category: "Running",
+        price: 329000,
+        imgUrl: "product-8-running-putih.png"
+    },
+]
+
+const ProductsSection = () => {
+    return (
+        <section id="product-section" className="container mx-auto mt-32">
+            <h2 className="font-bold italic text-4xl text-center mb-11">
+                <span className="text-primary">OUR</span> PRODUCTS
+            </h2>
+            <div className="grid grid-cols-4 gap-5">
+                {
+                    productList.map((product, index) => (
+                        <Link href="#" key={index} className="p-1.5 bg-white hover:drop-shadow-xl duration-300">
+                            <div className="bg-primary-light 
+                             aspect-square w-full flex justify-center items-center relative">
+                                <Image src={`/images/products/${product.imgUrl}`} 
+                                alt={product.name} 
+                                width={300}
+                                height={300}
+                                className="aspect-square object-contain" />
+                                <Button className="w=10 h-10 p-2! absolute right-3 top-3"><FiPlus size={24} /></Button>
+                            </div>
+                            <h3 className="font-medium text-lg mb-1.5 mt-4">{product.name}</h3>
+                            <div className="flex justify-between mb-8">
+                                <div className="text-gray-500">{product.category}</div>
+                                <div className="font-medium text-primary">{
+                                    Intl.NumberFormat("id-ID",{
+                                    style: "currency",
+                                    currency: "IDR",
+                                    maximumSignificantDigits: 3
+                                    }).format(product.price)}</div>
+                            </div>
+                        </Link>
+                    ))
+                }
+            </div>
+        </section>
+    );
+        
+};
+
+export default ProductsSection;
