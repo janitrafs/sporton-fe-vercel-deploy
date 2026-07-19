6f0b992901ba740e7612ac3b8115c2978fdc4e00
janitrafs <jxnitraa0100@gmail.com>
2026-07-19 09:23:15 +0700

    another update for session 2

--- app/(landing)/components/home/products.tsx
+++ app/(landing)/components/home/products.tsx
@@ -57,14 +57,14 @@ const productList = [
 
 const ProductsSection = () => {
     return (
-        <section id="product-section" className="container mx-auto mt-32 mb-52">
+        <section id="products-section" className="container mx-auto mt-32 mb-52">
             <h2 className="font-bold italic text-4xl text-center mb-11">
-                <span className="text-primary">OUR</span> PRODUCTS
+                <span className="text-primary">OUR </span> PRODUCTS
             </h2>
             <div className="grid grid-cols-4 gap-5">
                 {
                     productList.map((product, index) => (
-                        <Link href="#" key={index} className="p-1.5 bg-white hover:drop-shadow-xl duration-300">
+                        <Link href={`/product/${product.name}`} key={index} className="p-1.5 bg-white hover:drop-shadow-xl duration-300">
                             <div className="bg-primary-light 
                              aspect-square w-full flex justify-center items-center relative">
                                 <Image src={`/images/products/${product.imgUrl}`} 
@@ -72,7 +72,7 @@ const ProductsSection = () => {
                                 width={300}
                                 height={300}
                                 className="aspect-square object-contain" />
-                                <Button className="w=10 h-10 p-2! absolute right-3 top-3"><FiPlus size={24} /></Button>
+                                <Button className="w-10 h-10 p-2! absolute right-3 top-3"><FiPlus size={24} /></Button>
                             </div>
                             <h3 className="font-medium text-lg mb-1.5 mt-4">{product.name}</h3>
                             <div className="flex justify-between mb-8">
