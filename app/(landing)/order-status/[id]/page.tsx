5d532cee18cba198aa56cb37072a0dedfca140fd
janitrafs <jxnitraa0100@gmail.com>
2026-07-19 19:40:15 +0700

    fixing the order status page

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -8,10 +8,11 @@ const OrderStatus = () => {
     const [isConfirmed, setIsConfirmed] = useState(false);
     return (
         <main className="bg-gray-100 min-h-[80vh]">
-            <div className="max-w-5xl mx-auto py-10">
+            <div className="max-w-5xl mx-auto py-20">
                 <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
+                {isConfirmed ? <OrderConfirmed /> : <OrderSubmitted />}
             </div>
-            {isConfirmed ? <OrderConfirmed /> : <OrderSubmitted />}
+            
         </main>
     )
 }
