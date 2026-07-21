10ff9550cb7981d7e4f71d63f541c4b4a661bf45
janitrafs <jxnitraa0100@gmail.com>
2026-07-21 10:16:47 +0700

    fix: export TPageProps

--- app/(landing)/product/[id]/page.tsx
+++ app/(landing)/product/[id]/page.tsx
@@ -4,7 +4,7 @@ import priceFormatter from "@/app/utils/price-formatter";
 import { getProductDetails } from "@/app/services/product.service";
 import { getImageUrl } from "@/app/lib/api";
 
-type TPageProps = {
+export type TPageProps = {
     params: Promise<{id: string}>;
 }
 
@@ -31,7 +31,7 @@ const ProductDetail = async ({params}: TPageProps) => {
                 <div className="text-primary text-[32px] font-semibold mb-12">
                     {priceFormatter(product.price)}
                 </div>
-                <ProductActions stock={product.stock} />
+                <ProductActions product={product} stock={product.stock} />
             </div>
         </main>
     
